271bdc8a36ac1ad6796d371cefdad5b99e327a29
chillbok <kylejunho@gmail.com>
2026-03-12 12:58:45 +0900

    bool 변수 이름 수정 및 함수 작동 방식 변경
    3의 배수를 반환해야 하는지, 3의 배수를 반환해야 하는 게 아닌지를 매개변수를 받아 판단하도록 수정함.
    이를 통해 굳이 함수를 두개나 만들 필요가 없어졌음.

--- cppPractice/practice1/Problem1-1.c
+++ cppPractice/practice1/Problem1-1.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
-int multiples_of_3(int arr[], int size, int result[]) {
+int multiples_of_3(bool is_multiple_3, int arr[], int size, int result[]) {
     int result_count = 0;
     for (int i = 0; i < size; i++) {
-        if (arr[i] % 3 == 0) {
+        bool current_multiple3 = (arr[i] % 3 == 0);
+        if (current_multiple3 == is_multiple_3) {
             result[result_count] = arr[i];
             result_count += 1;
         }
@@ -15,7 +17,7 @@ int multiples_of_3(int arr[], int size, int result[]) {
 
 int main() {
     int size = 10;
-    int array[size], not_multiple3_array[size];
+    int array[size];
 
     srand(time(NULL));
     for (int i = 0; i < size; i++)
@@ -24,20 +26,30 @@ int main() {
         array[i] = num;
     }
     
-    int multiple3_array[size];
-    int multiple3_count = multiples_of_3(array, size, multiple3_array);
-    
-    // 배열 요소 출력
+    // 배열 출력
     printf("array: ");
     for (int i = 0; i < size; i++) {
         printf("%d, ", array[i]);
     }
     printf("\n");
     
+    // 3의 배수 담을 배열 생성하고, 배열에 담기
+    int multiple3_array[size];
+    int multiple3_count = multiples_of_3(true, array, size, multiple3_array);
+    
     printf("3의 배수: ");
     for (int i = 0; i < multiple3_count; i++) {
         printf("%d, ", multiple3_array[i]);
     }
     printf("\n");
+    
+    // 3의 배수가 아닌 숫자 담을 배열 생성하고, 배열에 담기
+    int not_multiple3_array[size];
+    int not_multiple3_count = multiples_of_3(false, array, size, not_multiple3_array);
+    
+    printf("3의 배수가 아닌 수: ");
+    for (int i = 0; i < not_multiple3_count; i++) {
+        printf("%d, ", not_multiple3_array[i]);
+    }
     return 0;
 }
