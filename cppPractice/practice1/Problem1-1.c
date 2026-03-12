4fdd6ab17c0e827adb09ce5e934efe38fecb39a3
chillbok <kylejunho@gmail.com>
2026-03-12 19:53:36 +0900

    3의 배수 세는 과정을 분리했음
    count_number() 함수는 is_multiple3 매개변수 여부에 따라 배열에서 3의 배수 개수, 3의 배수가 아닌 수 개수를 반환함.
    기존의 3의 배수 숫자를 따로 배열에 넣는 함수는 이름을 make_array_of_multiple3()로 수정했음.
    연산을 두번 수행하지만, 불필요한 메모리 낭비를 줄이기 위한 결정이었음.

--- cppPractice/practice1/Problem1-1.c
+++ cppPractice/practice1/Problem1-1.c
@@ -3,16 +3,27 @@
 #include <time.h>
 #include <stdbool.h>
 
-int multiples_of_3(bool is_multiple_3, int arr[], int size, int result[]) {
-    int result_count = 0;
-    for (int i = 0; i < size; i++) {
-        bool current_multiple3 = (arr[i] % 3 == 0);
-        if (current_multiple3 == is_multiple_3) {
-            result[result_count] = arr[i];
-            result_count += 1;
+int count_number(bool is_multiple3, int arr[], int arrSize) {
+    int result = 0;
+    for (int i = 0; i < arrSize; i++)
+    {
+        bool current_is_multiple3 = (arr[i] % 3 == 0);
+        if (current_is_multiple3 == is_multiple3) {
+            result += 1;
         }
     }
-    return result_count;
+    return result;
+}
+
+void make_array_of_multiple3(bool is_multiple3, int unsorted[], int unsorted_size, int result[], int result_size) {
+    int count = 0;
+    for (int i = 0; i < unsorted_size; i++) {
+        bool current_multiple3 = (unsorted[i] % 3 == 0);
+        if (current_multiple3 == is_multiple3) {
+            result[count] = unsorted[i];
+            count++;
+        }
+    }
 }
 
 int main() {
@@ -34,8 +45,9 @@ int main() {
     printf("\n");
     
     // 3의 배수 담을 배열 생성하고, 배열에 담기
-    int multiple3_array[size];
-    int multiple3_count = multiples_of_3(true, array, size, multiple3_array);
+    int multiple3_count = count_number(true, array, size);
+    int multiple3_array[multiple3_count];
+    make_array_of_multiple3(true, array, size, multiple3_array, multiple3_count);
     
     printf("3의 배수: ");
     for (int i = 0; i < multiple3_count; i++) {
@@ -47,10 +59,12 @@ int main() {
     int not_multiple3_array[size];
     int not_multiple3_count = multiples_of_3(false, array, size, not_multiple3_array);
     
+    /*
     printf("3의 배수가 아닌 수: ");
     for (int i = 0; i < not_multiple3_count; i++) {
         printf("%d ", not_multiple3_array[i]);
     }
     printf("\n");
+    */
     return 0;
 }
