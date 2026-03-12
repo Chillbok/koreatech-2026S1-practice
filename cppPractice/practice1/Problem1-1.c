692b0e872776a9b160c23fdd358426544772a250
chillbok <kylejunho@gmail.com>
2026-03-12 12:13:12 +0900

    랜덤으로 숫자 만들어서 넣는 기능 구현

--- cppPractice/practice1/Problem1-1.c
+++ cppPractice/practice1/Problem1-1.c
@@ -5,5 +5,12 @@
 int main() {
     int size = 10;
     int array[size], multiple3_array[size], not_multiple3_array[size];
+
+    srand(time(NULL));
+    for (int i = 0; i < size; i++)
+    {
+        int num = rand() % 101;
+        array[i] = num;
+    }
     return 0;
 }
