78eb7a05f040a254691353d22df4abfcdfe65fca
chillbok <kylejunho@gmail.com>
2026-03-15 22:20:29 +0900

    데이터를 담기 위한 구조체 생성
    TestCase 구조체에는 정수의 개수를 담기 위한 int 변수와 데이터를 담기 위한 int 벡터가 있음.

--- algorithmPractice/chapter1/B.cpp
+++ algorithmPractice/chapter1/B.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <vector>
+
+// 이번에 배워서 처음 써보는 구조체
+struct TestCase {
+    int integer_count;
+    std::vector<int>data;
+};
 
 int main() {
+    using std::vector;
+    using std::cin;
+    using std::cout;
+    using std::endl;
+
+    int testcase_count;
+    cin >> testcase_count;
+    TestCase user_input[testcase_count];
     return 0;
 }
