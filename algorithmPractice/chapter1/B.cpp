06c26ab5a285c4dc6a07e487bf9fc726c6cec3a1
chillbok <kylejunho@gmail.com>
2026-03-16 00:00:58 +0900

    완성했으나 제대로 작동하지 않음
    일단 기능은 전부 완성했는데, 결과가 전부 0으로 출력됨.
    따라서 main() 함수에서 관련 부분들은 전부 주석처리했음.

--- algorithmPractice/chapter1/B.cpp
+++ algorithmPractice/chapter1/B.cpp
@@ -18,13 +18,37 @@ void fill_vector(std::vector<int>& param_vector) {
 }
 
 // 요구한 계산 진행하는 함수
-int calculate(int n, int i, int j, int k) {
+int calculate(int count, int i, int j, int k) {
+    int n = count;
     int result = n * n * k * (i - j);
     if (result < 0) result = 0;
     return result;
 }
 
 // 번호 3개 고른 뒤 해당 테스트케이스에서 계산값 가장 큰 것 찾는 함수
+void select_number_and_find_biggest(TestCase testcase) {
+    using std::vector;
+    int& biggest_calculated = testcase.biggest_calculated;
+    biggest_calculated = 0;
+    vector<int>& v = testcase.data;
+    int v_size = v.size();
+    int selected[3];
+    for (int i = 0; i < v_size; i++) {
+        selected[0] = v[i];
+        for (int j = 0; j < v_size; j++) {
+            // 앞 수보다 작거나 같으면 건너뛰기
+            if (v[j] <= selected[0]) continue;
+            selected[1] = v[j];
+            for (int k = 0; k < v_size; k++) {
+                // 앞 수보다 작거나 같으면 건너뛰기
+                if (v[k] <= selected[1]) continue;
+                selected[2] = v[k];
+                int value = calculate(v_size, selected[0], selected[1], selected[2]);
+                if (value > biggest_calculated) biggest_calculated = value;
+            }
+        }
+    }
+}
 
 int main() {
     using std::vector;
@@ -46,6 +70,12 @@ int main() {
     }
     
     // 변호 3개 고르고, 요구한 계산 진행하는 함수 아래에 적어야 함
+    /*
+    for (int i = 0; i < testcase_count; i++) {
+        select_number_and_find_biggest(user_input[i]);
+        cout << user_input[i].biggest_calculated << endl;
+    }
+    */
 
     return 0;
 }
