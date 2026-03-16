9c36b558db5e4e70e12f0351ffa5fb7748c3f449
chillbok <kylejunho@gmail.com>
2026-03-17 03:19:45 +0900

    even, odd 배열 생성 위치를 input 배열 생성 시점으로 수정

--- cppPractice/practice1/Problem1-2.c
+++ cppPractice/practice1/Problem1-2.c
@@ -10,16 +10,16 @@ int main() {
     int arr_size = 10;
     // 일단 입력 받는 배열
     int input[arr_size];
+    // 짝수 담는 배열
+    int even[arr_size];
+    // 홀수 담는 배열
+    int odd[arr_size];
     
     // 하나씩 숫자 입력
     for (int i = 0; i < arr_size; i++) {
         printf("%d번째 숫자(하나씩 숫자 입력): ", i+1);
         scanf("%d", &input[i]);
     }
-    // 짝수
-    int even[arr_size];
-    // 홀수
-    int odd[arr_size];
     int even_count = 0;
     int odd_count = 0;
     // 짝수 홀수 나눠서 각각 배열에 넣기
