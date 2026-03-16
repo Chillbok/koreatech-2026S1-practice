4f1d5cd1814b2cdf152277fcaca381e4e1987e50
chillbok <kylejunho@gmail.com>
2026-03-16 10:49:39 +0900

    데이터 입력하는 부분까지는 구현함
    TestCase 구조체를 추가했음.
    구조체에는 데이터를 담는 벡터, 정리한 값을 담는 벡터 총 2개가 포함됨.
    main() 함수에서 데이터를 입력하는 것까지는 완료함.

--- algorithmPractice/C.cpp
+++ algorithmPractice/C.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+struct TestCase {
+    std::vector<int> data;
+    std::vector<int> sorted;
+};
 
 int main() {
+    int testcase_size;
+    cin >> testcase_size;
+    std::vector<TestCase> inputs;
+    inputs.resize(testcase_size);
+    
+    for (int i = 0; i < testcase_size; i++) {
+        int numbers_amount;
+        cin >> numbers_amount;
+        inputs[i].data.resize(numbers_amount);
+
+        for (int j = 0; j < numbers_amount; j++) {
+            cin  >> inputs[i].data[j];
+        }
+    }
+
     return 0;
 }
