d1b7c71c7c3158f55446b0e0533bf0b9634de42b
chillbok <kylejunho@gmail.com>
2026-03-16 14:02:48 +0900

    하나씩 입력 받아 배열에 저장하는 코드 구현

--- cppPractice/practice1/Problem1-2.c
+++ cppPractice/practice1/Problem1-2.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int input[10];
+    int arr_size = 10;
+    // 일단 입력 받는 배열
+    int input[arr_size];
+    
+    // 하나씩 숫자 입력
+    for (int i = 0; i < arr_size; i++) {
+        printf("%d번째 숫자(하나씩 숫자 입력): ", i+1);
+        scanf("%d", &input[i]);
+    }
+    // 짝수
+    int even[arr_size];
+    // 홀수
+    int odd[arr_size];
     return 0;
 }
