538f945fb212ad0bf719352a722145d4e02fe9d0
chillbok <kylejunho@gmail.com>
2026-03-17 17:46:31 +0900

    기존 파일이 답은 나오는데, 시간 초과되어 새로운 파일 만들어봄
    이번에도 시간 초과가 나왔는데, 어떻게 줄여야 할지 모르겠음.

--- algorithmPractice/chapter1/A.cpp
+++ algorithmPractice/chapter1/A.cpp
@@ -1,101 +1,72 @@
 #include <iostream>
 #include <vector>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
 
-// 절댓값 크기 차이가 1 이하인지 판단하는 함수
-bool define_harmony_number(int& first_number, int& selected_number)
-{
-    int gap = first_number - selected_number;
-    if (gap >= -1 || gap <= 1) return true;
-    else return false;
+struct TestCase {
+    vector<int> input;
+    vector<int> longest_vector;
+};
+
+vector<int> get_longer_vector(vector<int> v1, vector<int> v2) {
+    if (v1.size() < v2.size()) return v2;
+    else return v1;
 }
 
-// 매개변수로 지정한 벡터에서 특정 수를 기준으로 1만큼 크거나 작은 수를 찾아 벡터에 집어넣은 뒤 길이 반환하는 변수 
-int make_and_get_harmony_vector_length(bool is_smallest, std::vector<int>& param_vector, int first_number)
-{
-    int param_length = param_vector.size();
-    int smallest = first_number;
-    int biggest = smallest;
-    std::vector<int> result;
-    // 제일 작은거 기준으로 생각할 때
-    if (is_smallest)
-    {
-        for (int i = 0; i < param_length; i++)
+// 숫자 두 개 중 하나가 있으면 벡터에 넣어서 조화시퀸스 만들어서 반환하는 함수
+// 만약 조화시퀸스를 만들지 못했다면 0 하나만 들어있는 벡터 반환
+vector<int> get_longer_harmony_vector(vector<int> v, int selected_number, int addable_number) {
+    vector<int> result;
+    bool usable = false;
+    for (int i = 0; i < v.size(); i++) {
+        if (v[i] == selected_number) result.push_back(v[i]);
+        else if (v[i] == addable_number)
         {
-            if (param_vector[i] == smallest) result.push_back(param_vector[i]);
-            else if (param_vector[i] == smallest + 1)
-            {
-                result.push_back(param_vector[i]);
-                biggest = param_vector[i];
-            }
-        }
-    }
-    // 제일 큰거 기준으로 생각할 때
-    else if (!is_smallest)
-    {
-        for (int i = 0; i < param_length; i++)
-        {
-            if (param_vector[i] == biggest) result.push_back(param_vector[i]);
-            else if (param_vector[i] == biggest - 1)
-            {
-                result.push_back(param_vector[i]);
-                smallest = param_vector[i];
-            }
+            if (usable == false) usable = true;
+            result.push_back(v[i]);
         }
     }
     
-    // 제일 큰거, 제일 작은거 크기 차이가 1이 아니면 0 반환
-    if (biggest - smallest != 1) return 0;
-    return result.size();
+    if (usable == true) return result;
+    vector<int> unusable_result;
+    return unusable_result;
 }
 
-// 입력한 벡터의 조화배열 만들고, 가장 긴 조화배열 길이 반환하는 변수
-int get_longest_harmony_vector_length(std::vector<int>& param_vector) 
-{
-    // 매개변수로 받은 벡터 크기
-    int biggest_length = 0;
-    int param_vector_length = param_vector.size();
-    int smallest, biggest;
-    std::vector<int> first_smallest;
-    std::vector<int> first_biggest;
-    int longest_length = 0;
-    for (int i = 0; i < param_vector_length; i++)
-    {
-        // NOTE: 이거 그냥 길이 반환하는 함수에서 두 경우 모두 만들어보고 비교해보면 안되나?
-        // 작은 수 기준으로 조화배열 만든 경우의 수
-        int temporary_length = make_and_get_harmony_vector_length(true, param_vector, param_vector[i]);
-        if (longest_length < temporary_length) longest_length = temporary_length;
-        // 큰 수 기준으로 조화배열 만든 경우의 수
-        temporary_length = make_and_get_harmony_vector_length(false, param_vector, param_vector[i]);
-        if (longest_length < temporary_length) longest_length = temporary_length;
+// 테스트케이스를 받으면, 그 테스트케이스에서 만들 수 있는 가장 조화시퀸스 만들어서 테스트케이스 구조체에 집어넣는 함수반환하는 함수
+void get_longest_harmony_sequence(TestCase& tc) {
+    // 테스트케이스를 받으면 두 개의 벡터를 만들어야 함
+    for (int i = 0; i < tc.input.size(); i++) {
+        vector<int> smaller_than_first_index = get_longer_harmony_vector(tc.input, tc.input[i], tc.input[i] - 1);
+        vector<int> bigger_than_first_index = get_longer_harmony_vector(tc.input, tc.input[i], tc.input[i] + 1);
+        vector<int> longer_vector = get_longer_vector(smaller_than_first_index, bigger_than_first_index);
+        if (tc.longest_vector.size() < longer_vector.size()) tc.longest_vector = longer_vector;
     }
-    return longest_length;
 }
 
-int main()
-{
-    int x_length, y_length;
-    std::cin >> x_length;
-
-    //입력한 배열들 저장하는 변수
-    std::vector<std::vector<int>> inputs(x_length);
-
-    for (int i = 0; i < x_length; i++)
-    {
-        std::cin >> y_length;
-        inputs[i].resize(y_length);
-        
-        for (int j = 0; j < y_length; j++)
-        {
-            std::cin >> inputs[i][j];
+int main() {
+    // 테스트케이스 개수 입력받고, 테스트케이스 배열 생성
+    int testcase_count;
+    cin >> testcase_count;
+    vector<TestCase> testcases(testcase_count);
+    
+    // 각 테스트케이스의 크기 설정하고, 값 입력받기
+    for (int i = 0; i < testcase_count; i++) {
+        int v_length;
+        cin >> v_length;
+        vector<int>& v = testcases[i].input;
+        v.resize(v_length);
+        for (int j = 0; j < v_length; j++) {
+            cin >> v[j];
         }
     }
     
-    int length = 0;
-    // 배열 개수만큼 수 연산하고, 연산한 수는 바로 출력
-    for (int i = 0; i < inputs.size(); i++)
-    {
-        length = get_longest_harmony_vector_length(inputs[i]);
-        std::cout << length << std::endl;
+    // 테스트케이스에서 가장 긴 조화시퀸스 만들기
+    for (int i = 0; i < testcases.size(); i++) {
+        get_longest_harmony_sequence(testcases[i]);
+        cout << testcases[i].longest_vector.size() << endl;
     }
+    
     return 0;
 }
