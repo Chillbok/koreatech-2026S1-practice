dbe6ee6540ad060a565f2cd41e3396d3a50f1706
chillbok <kylejunho@gmail.com>
2026-03-19 01:20:57 +0900

    A.cpp ver1 완성: 재귀호출을 사용하여 모든 경우의 수를 판단하도록 설정
    solve 함수에는 4개의 if문이 있음.
    첫 2개의 조건문은 완성 또는 조합 없음을 판단하는 조건문임.
    나중 2개의 조건문은 아직 남은 경우의 수가 있다면 계속해서 새로운 조건문을 실행하도록 하는 분기 생성 조건문임.

--- algorithmPractice/chapter2/A.cpp
+++ algorithmPractice/chapter2/A.cpp
@@ -5,15 +5,18 @@ using std::cout;
 using std::endl;
 using std::vector;
 
-bool solve(vector<int> numbers, int number_to_make) {
-    if (number_to_make == 0) return true;
-    int size = numbers.size();
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            if (j == i) continue;
-            if (numbers[i] + numbers[j] == number_to_make) return true;
-        }
-    }
+bool solve(vector<int> numbers, int target, int index) {
+    // 공집합인 경우
+    if (target == 0) return true;
+    
+    // 끝까지 확인했는데 못맞춘 경우, 목표가 음수가 된 경우
+    if (index >= numbers.size() || target < 0) return false;
+
+    // 현재 숫자를 포함하는 경우
+    if (solve(numbers, target - numbers[index], index + 1)) return true;
+    
+    // 현재 숫자를 포함하지 않는 경우
+    if (solve(numbers, target, index + 1)) return true;
     return false;
 }
 
@@ -33,7 +36,7 @@ int main() {
             cin >> selected_integer;
         }
         
-        testcase_result.push_back(solve(input_integers, m));
+        testcase_result.push_back(solve(input_integers, m, 0));
     }
     
     for (bool const& x : testcase_result) {
