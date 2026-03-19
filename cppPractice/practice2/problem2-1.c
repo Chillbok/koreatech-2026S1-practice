88931cc240dc76f2503a9937a3a5937b1c284d2b
chillbok <kylejunho@gmail.com>
2026-03-19 12:07:41 +0900

    점수를 표로 표시하는 것까지 성공
    그러나 float의 소숫값이 6자리나 출력되는 문제가 발생하고 있음.
    소숫값이 지나치게 많아서 숫자도 길게 출력되고, 맨 상단의 과목명과 일치하지 않는 문제가 발생하고 있음.
    또한 왼쪽 정렬로 되어 있는 상태인데, 이걸 어떻게 해결해야 할지가 문제임.

--- cppPractice/practice2/problem2-1.c
+++ cppPractice/practice2/problem2-1.c
@@ -1,5 +1,42 @@
 #include <stdio.h>
 
+void print_scores(const char* student_names[], const float scores[][4], const char* score_names[], const int student_count, const int score_count) {
+    // 첫줄 작성
+    printf("\t");
+    printf("|");
+
+    // 첫 열 출력
+    for (int i = 0; i < score_count; i++) {
+        printf("%s\t", score_names[i]);
+        printf("|");
+    }
+    printf("\n");
+
+    // 점수들 출력하기
+    for (int i = 0; i < student_count; i++) {
+        printf("%s\t", student_names[i]);
+        printf("|");
+        for (int j = 0; j < score_count; j++) {
+            printf("%f\t", scores[i][j]);
+            printf("|");
+        }
+        printf("\n");
+    }
+}
+
 int main() {
+    int student_count = 4;
+    int scores_count = 4;
+    // 문자열 안전하게 출력하기 위해 빈칸 하나 추가함
+    const char* student_names[] = { "철희", "철수", "영희", "영수" };
+    const char* score_names[] = { "국어", "영어", "수학", "도덕" };
+    float scores[student_count][scores_count];
+    for (int i = 0; i < student_count; i++) {
+        for (int j = 0; j < scores_count; j++) {
+            printf("%s의 %s 점수를 입력하세요. : \n", student_names[i], score_names[j]);
+            scanf("%f", &scores[i][j]);
+        }
+    }
+    print_scores(student_names, scores, score_names, student_count, scores_count);
     return 0;
 }
