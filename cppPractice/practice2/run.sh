2e6ad123c5e873ac8289255c59bc55add323f26d
chillbok <kylejunho@gmail.com>
2026-03-20 10:01:48 +0900

    run.sh 작동하지 않는 문제 해결함
    bash 스크립트가 띄어쓰기에 민감하다는 사실을 처음 알았음.

--- cppPractice/practice2/run.sh
+++ cppPractice/practice2/run.sh
@@ -2,17 +2,17 @@
 
 SOURCE_FILE=$1
 
-if [ -z "$SOURCE_FILE"]; then
+if [ -z "$SOURCE_FILE" ]; then
     echo "사용법: ./run.sh 파일이름.cpp 또는 ./run.sh 파일이름.c"
     exit 1
 fi
 
 if [[ "$SOURCE_FILE" == *.c ]]; then
-    COMPILER = "gcc"
-    echo "C 파일로 인식해 $COMPILER를 사용함."
+    COMPILER="gcc"
+    echo "C 파일로 인식해 $COMPILER 를 사용함."
 elif [[ "$SOURCE_FILE" == *.cpp ]]; then
-    COMPILER = "g++"
-    echo "C++ 파일로 인식해 $COMPILER를 사용함."
+    COMPILER="g++"
+    echo "C++ 파일로 인식해 $COMPILER 를 사용함."
 else "지원하지 않는 파일 형식"
     exit 1
 fi
