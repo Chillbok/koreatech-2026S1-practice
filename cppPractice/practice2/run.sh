bd736f94a75187e571d79b6239e18ac2ccd1509f
chillbok <kylejunho@gmail.com>
2026-03-20 09:57:15 +0900

    run.sh 파일에 각 파일 형식별로 사용하는 컴파일러가 다르도록 설정함

--- cppPractice/practice2/run.sh
+++ cppPractice/practice2/run.sh
@@ -5,5 +5,16 @@ SOURCE_FILE=$1
 if [ -z "$SOURCE_FILE"]; then
     echo "사용법: ./run.sh 파일이름.cpp 또는 ./run.sh 파일이름.c"
     exit 1
-SOURCE_FILE
-g++ "$SOURCE_FILE" -o program.out && ./program.out
+fi
+
+if [[ "$SOURCE_FILE" == *.c ]]; then
+    COMPILER = "gcc"
+    echo "C 파일로 인식해 $COMPILER를 사용함."
+elif [[ "$SOURCE_FILE" == *.cpp ]]; then
+    COMPILER = "g++"
+    echo "C++ 파일로 인식해 $COMPILER를 사용함."
+else "지원하지 않는 파일 형식"
+    exit 1
+fi
+
+$COMPILER "$SOURCE_FILE" -o program.out && ./program.out
