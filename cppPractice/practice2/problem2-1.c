6d4c08b1b9ed5b19e52ff75fc2321578daf47f6f
chillbok <kylejunho@gmail.com>
2026-03-20 12:36:50 +0900

    problem2-1(v0.1): 일단 모든 방식을 매직넘버를 활용하는 방식으로 수정
    나중에 한꺼번에 수정하든가 해야겠음.

--- cppPractice/practice2/problem2-1.c
+++ cppPractice/practice2/problem2-1.c
@@ -30,29 +30,36 @@ void print_scores(const char* student_names[], const float scores[][4], const ch
 }
 
 int main() {
-    int student_count = 4;
-    int score_type_count = 4;
     // 문자열 안전하게 출력하기 위해 빈칸 하나 추가함
     const char* student_names[] = { "철희", "철수", "영희", "영수" };
     const char* score_type_names[] = { "국어", "영어", "수학", "도덕" };
-    float scores[student_count][score_type_count];
-    for (int i = 0; i < student_count; i++) {
-        for (int j = 0; j < score_type_count; j++) {
+    float scores[4][4];
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
             printf("%s의 %s 점수를 입력하세요. : \n", student_names[i], score_type_names[j]);
             scanf("%f", &scores[i][j]);
         }
     }
-    print_scores(student_names, scores, score_type_names, student_count, score_type_count);
+    print_scores(student_names, scores, score_type_names, 4, 4);
     
-    // 점수별 평균 구하기
-    float score_average[score_type_count];
-    for (int i = 0; i < score_type_count; i++) score_average[i] = 0;
-    for (int i = 0; i < student_count; i++) {
-        for (int j = 0; j < score_type_count; j++) {
+    // 학생별 평균 구하기
+    float score_average[4];
+    for (int i = 0; i < 4; i++) score_average[i] = 0;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
             score_average[j] = scores[i][j];
         }
     }
-    for (int i = 0; i < score_type_count; i++) score_average[i] /= student_count;
+    for (int i = 0; i < 4; i++) score_average[i] /= 4;
+    
+    // 과목 평균 구하기
+    float subject_average[] = {0, 0, 0, 0};
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            score_average[i] += scores[j][i];
+        }
+        score_average[i] /= 4;
+    }
 
     return 0;
 }
