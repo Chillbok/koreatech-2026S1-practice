981c20154949276ca592561a876b24146ef35855
chillbok <kylejunho@gmail.com>
2026-03-25 01:08:41 +0900

    practice2(A): 다른 방법을 시도해봤으나, 이번에도 안됨
    교재 보고 한건데 안됨.

--- algorithmPractice/chapter2/A.cpp
+++ algorithmPractice/chapter2/A.cpp
@@ -6,12 +6,15 @@ using std::endl;
 using std::vector;
 
 bool solve(const vector<int>& numbers, int target, int index) {
+    if (target < 0) return false;
     // 공집합이거나, 조합을 완성한 경우
     if (target == 0) return true;
     //끝까지 확인했는데 못 맞춘 경우거나, 목표가 음수가 된 경우
-    if (index >= numbers.size() || target < 0) return false;
-    // 분기 생성 조건문들
-    return solve(numbers, target - numbers[index], index + 1) || solve(numbers, target, index + 1);
+    if (index >= numbers.size()) return false;
+    for (const int& selected : numbers) {
+        if(solve(numbers, target - selected, index + 1)) return true;
+    }
+    return false;
 }
 
 int main() {
@@ -20,18 +23,16 @@ int main() {
     
     int testcase_length;
     cin >> testcase_length;
-    vector<bool> testcase_result;
-    for (int i = 0; i < testcase_length; i++) {
+
+    while(testcase_length--) {
         int m, n;
         cin >> m >> n;
         vector<int> input_integers(n);
-        for (int& selected_integer : input_integers) {
-            cin >> selected_integer;
+        for (int& selected : input_integers) {
+            cin >> selected;
         }
-        
-        // 결과 즉시 출력
-        if (solve(input_integers, m, 0)) cout << "true\n";
-        else cout << "false\n";
+        if (solve(input_integers, m, 0)) cout << "true" << endl;
+        else cout << "false" << endl;
     }
 
     return 0;
