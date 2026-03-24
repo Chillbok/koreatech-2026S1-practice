312dbd9c039c36aa709dae51f9de3b97a612db99
chillbok <kylejunho@gmail.com>
2026-03-25 01:17:59 +0900

    practice2(B): 첫 커밋

---
+++ algorithmPractice/chapter2/B.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+
+bool solve(int& m, vector<int> A) {
+    
+}
+
+int main() {
+    
+    return 0;
+}
