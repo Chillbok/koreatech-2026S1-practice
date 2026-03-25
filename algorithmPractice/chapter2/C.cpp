2e38804f6de6cb2fc881b7fa911357c2a5c81d4b
chillbok <kylejunho@gmail.com>
2026-03-25 20:01:04 +0900

    practice2(C): 완성

--- algorithmPractice/chapter2/C.cpp
+++ algorithmPractice/chapter2/C.cpp
@@ -5,44 +5,26 @@ using std::cout;
 using std::endl;
 using std::vector;
 
-vector<int>* solve(int target, vector<int> testcase) {
-    if (target == 0) return new vector<int>();
-    if (target < 0) return nullptr;
-    vector<int>* best_case = nullptr;
-    
-    for (int& x : testcase) {
-        vector<int>* current_case = solve(target - x, testcase);
-        if (current_case != nullptr) {
-            if (best_case == nullptr || (best_case->size() > (current_case->size() + 1))) {
-                current_case->push_back(x);
-                delete best_case;
-                best_case = current_case;
-            }
-        }
-    }
-    return best_case;
+int get_count(int target, vector<int> testcase) {
+    if (target < 0) return 0;
+    if (target == 0) return 1;
+    int count = 0;
+    for (const int& x : testcase) count += get_count(target - x, testcase);
+    return count;
+}
+
+void test() {
+    int target, count;
+    cin >> target >> count;
+    vector<int> testcase(count);
+    for (int& x : testcase) cin >> x;
+    int result = get_count(target, testcase);
+    cout << result << endl;
 }
 
 int main() {
     int testcase_count;
     cin >> testcase_count;
-    
-    for (int i = 0; i < testcase_count; i++) {
-        int target, integer_count;
-        cin >> target >> integer_count;
-
-        vector<int> testcase(integer_count);
-        for (int& idx : testcase) cin >> idx;
-        // 경우의 수 출력
-        vector<int>* result = solve(target, testcase);
-        if (result != nullptr) {
-            cout << result->size() << " ";
-            for (int& x : *result) cout << x << " ";
-            cout << endl;
-        }
-        else cout << -1 << endl;
-        delete result;
-    }
-    
+    while (testcase_count--) test();
     return 0;
 }
