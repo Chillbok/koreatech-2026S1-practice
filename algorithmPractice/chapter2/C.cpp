520f0807f54fd9fdc07060ac79c44135c4cf4175
chillbok <kylejunho@gmail.com>
2026-03-25 10:19:56 +0900

    practice2(C): 기초 함수 생성

---
+++ algorithmPractice/chapter2/C.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+
+vector<int>* solve() {
+
+}
+
+int main() {
+    int testcase_count;
+    cin >> testcase_count;
+    
+    while (testcase_count--) {
+        int M, N;
+        vector<int> testcase(N);
+        for (int& idx : testcase) cin >> idx;
+        // 경우의 수 출력
+    }
+    
+    return 0;
+}
