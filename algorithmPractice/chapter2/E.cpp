3b372ea97cf3e7bf9a8e935857fba65476e7b852
chillbok <kylejunho@gmail.com>
2026-03-25 22:11:50 +0900

    practice2(E): 필요할 것 같은 함수들 추가
    test() 함수는 한번의 테스트케이스를 실행하는 함수임.
    solve() 함수는 입력값을 기반으로 연산하여 결과를 도출하는 함수임.

--- algorithmPractice/chapter2/E.cpp
+++ algorithmPractice/chapter2/E.cpp
@@ -3,8 +3,19 @@
 using std::cin, std::cout, std::endl;
 using std::vector;
 
+bool solve() {
+    
+}
+
+void test() {
+    int var_count;
+    cin >> var_count;
+    // 값 입력받고, 그 입력받은 값을 기반으로 계산하는 solve() 함수 아래에서 선언할 것
+}
+
 int main() {
     int testcase_count;
     cin >> testcase_count;
+    while (testcase_count--) test();
     return 0;
 }
