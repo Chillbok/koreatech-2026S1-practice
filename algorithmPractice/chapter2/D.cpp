cf936dc7dc9f06b6426692b12f98f23507d7ecfa
chillbok <kylejunho@gmail.com>
2026-03-25 21:07:35 +0900

    practice2(D): 재귀함수 완성

--- algorithmPractice/chapter2/D.cpp
+++ algorithmPractice/chapter2/D.cpp
@@ -3,6 +3,23 @@
 using std::cin; using std::cout; using std::endl;
 using std::vector;
 
+vector<int>* sum(int target, vector<int> testcase) {
+    if (target < 0) return nullptr;
+    if (target == 0) return new vector<int>();
+    vector<int>* best_case = nullptr;
+    
+    for (int& x : testcase) {
+        vector<int>* current_case = sum(target - x, testcase);
+        if (current_case != nullptr) {
+            if (best_case == nullptr || best_case->size() > (current_case->size() + 1)) {
+                current_case->push_back(x);
+                best_case = current_case;
+            }
+        }
+    }
+    return best_case;
+}
+
 int main() {
     return 0;
 }
