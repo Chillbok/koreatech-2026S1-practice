f44448d34747edf76e69eb60f8fdef93e40facaa
chillbok <kylejunho@gmail.com>
2026-03-26 21:28:49 +0900

    practice3(F): 임시로 테스트 함수 생성

--- algorithmPractice/chapter2/F.cpp
+++ algorithmPractice/chapter2/F.cpp
@@ -3,6 +3,18 @@ using std::cin, std::cout, std::endl;
 #include <vector>
 using std::vector;
 
+bool check(const char* s, long long a, long long b, int start_idx) {
+    if (start_idx == strlen(s)) return true;
+    
+    long long c = a + b;
+    char c_str[20];
+    sprintf(c_str, "%lld", c);
+    int len = strlen(c_str);
+    
+    if (strncmp(s + start_idx, c_str, len) == 0) return check(s, b, c, start_idx + len);
+    return false;
+}
+
 int main() {
     int testcase_count;
     cin >> testcase_count;
