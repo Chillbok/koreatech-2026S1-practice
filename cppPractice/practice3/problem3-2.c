f4a9e8aba2ae75fcf2e6481c1d8e1d4885e47569
chillbok <kylejunho@gmail.com>
2026-03-26 13:20:03 +0900

    p3-2(v0.1): 기본 템플릿으로 구성되는 코드 작성

--- cppPractice/practice3/problem3-2.c
+++ cppPractice/practice3/problem3-2.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char* reverse_string(const char* src, int len) {
+    
+}
+
 int main() {
+    char source[] = "InternetMedia Engineering";
+    char* result = reverse_string(source, strlen(source));
+    printf("원본 문자열: %s\n", source);
+    printf("변경된 문자열: %s\n", result);
+    free(result);
     return 0;
 }
