ef25107f55f903d19ac850ae844536b8c2226630
chillbok <kylejunho@gmail.com>
2026-03-26 23:12:04 +0900

    chapter3(C): 첫 커밋
    필요한 주석을 추가했음.

---
+++ algorithmPractice/chapter3/C.cpp
@@ -0,0 +1,14 @@
+/*
+모든 문제를 분할 정복 기법을 이용해 해결해야 함.
+더 효과적인 방법이 있더라도, 반드시 강의 슬라이드에 제시된 알고리즘을 활용해 완성할 것.
+*/
+// C번 출력 방법:
+// printf("%.2f\n", answer); // C++
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    return 0;
+}
