269ff383bd480a738e0aacde96b9e7e810fc2bcc
chillbok <kylejunho@gmail.com>
2026-03-26 23:33:16 +0900

    practice3(A): 첫 커밋

---
+++ algorithmPractice/chapter3/A.cpp
@@ -0,0 +1,14 @@
+/*
+모든 문제를 분할 정복 기법을 이용해 해결해야 함.
+더 효과적인 방법이 있더라도, 반드시 강의 슬라이드에 제시된 알고리즘을 활용해 완성할 것.
+*/
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    int testcase_count;
+    if (!(cin >> testcase_count)) return 1;
+    return 0;
+}
