9a49de5ccc51eaa1566e938be612458f72f87de5
chillbok <kylejunho@gmail.com>
2026-03-27 01:43:54 +0900

    practice3(B): 각 케이스의 입력 받는 코드 완성
    각 케이스를 입력받아 벡터를 만듬.

--- algorithmPractice/chapter3/B.cpp
+++ algorithmPractice/chapter3/B.cpp
@@ -3,9 +3,17 @@ using std::cin; using std::cout; using std::endl;
 #include <vector>
 using std::vector;
 
+void test() {
+    int case_length;
+    cin >> case_length;
+    vector<int> cases(case_length);
+    for (int& x : cases) cin >> x;
+}
+
 int main() {
     int testcase_count;
     if (!(cin >> testcase_count)) return 0;
     
+    while (testcase_count--) test();
     return 0;
 }
