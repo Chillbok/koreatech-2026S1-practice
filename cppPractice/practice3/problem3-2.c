20391f91b608bcfc40baaad401b2b3d1f692b16d
chillbok <kylejunho@gmail.com>
2026-03-26 13:53:38 +0900

    cpp3-2(v0.1): 문자열 길이 구하는 함수 작성
    get_string_length() 함수는 문자열의 길이를 반환함.
    배열의 길이가 아니라, 맨 뒤의 '\0'을 출력하는 값은 제외하고 센다는 사실을 명심해야 함.
    문자열이 영어만 있을 때에만 올바른 값이 출력됨.
    따라서 한글과 영어를 구분하는 기능을 구현해야 할 것임.

--- cppPractice/practice3/problem3-2.c
+++ cppPractice/practice3/problem3-2.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int get_string_length(const char* s) {
+    int count = 0;
+    const char* word = s;
+    while (*word != '\0') {
+        word++;
+        count++;
+    }
+    // 맨 뒤의 null 값은 제외되어 출력되기 때문에, 잘 생각해야 함.
+    // 예를 들어, "hello"를 출력한다면, 5가 출력될 것임. 하지만 배열의 실제 길이는 6임.
+    return count;
+}
+
 char* reverse_string(const char* src, int len) {
     
 }
