39c7f8d8b5c05d62cd1c326f5a967556be1f5e84
chillbok <kylejunho@gmail.com>
2026-03-27 00:05:11 +0900

    practice3(A): 코드 완성

--- algorithmPractice/chapter3/A.cpp
+++ algorithmPractice/chapter3/A.cpp
@@ -9,9 +9,49 @@ using std::endl;
 #include <vector>
 using std::vector;
 
+long long merge_and_count(vector<int>& arr, int left, int mid, int right) {
+    vector<int> temp(right - left + 1);
+    int i = left, j = mid + 1, k = 0;
+    long long split_inv = 0;
+
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j]) {
+            temp[k++] = arr[i++];
+        }
+        else {
+            temp[k++] = arr[j++];
+            split_inv += (mid -i + 1);
+        }
+    }
+    
+    while (i <= mid) temp[k++] = arr[i++];
+    while (j <= right) temp[k++] = arr[j++];
+    for (int p = 0; p < temp.size(); p++) arr[left + p] = temp[p];
+    
+    return split_inv;
+}
+
+long long sort_count_inv(vector<int>& arr, int left, int right) {
+    if (left >= right) return 0;
+    int mid = left + (right - left) / 2;
+
+    long long L = sort_count_inv(arr, left, mid);
+    long long R = sort_count_inv(arr, mid + 1, right);
+    long long S = merge_and_count(arr, left, mid, right);
+
+    return L + R + S;
+}
 
 int main() {
     int testcase_count;
     if (!(cin >> testcase_count)) return 1;
+    while (testcase_count--) {
+        int arr_size;
+        cin >> arr_size;
+        vector<int> arr(arr_size);
+        for (int i = 0; i < arr_size; i++) cin >> arr[i];
+        cout << sort_count_inv(arr, 0, arr_size - 1) << "\n";
+    }
+
     return 0;
 }
