96084936e47053cbf24e41f75fb77157ec5eb1a9
chillbok <kylejunho@gmail.com>
2026-03-27 01:37:01 +0900

    practice3(B): 첫 커밋

---
+++ algorithmPractice/chapter3/B.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using std::cin; using std::cout; using std::endl;
+#include <vector>
+using std::vector;
+
+int main() {
+    int testcase_count;
+    if (!(cin >> testcase_count)) return 0;
+    
+    return 0;
+}
