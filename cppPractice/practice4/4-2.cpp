6cddd30ec4e10df28d2806675ebbdff7847a8e25
chillbok <kylejunho@gmail.com>
2026-04-02 21:29:04 +0900

    cpp-4.2.0.2: 이름 배열 길이와 점수 길이를 상수화
    이름 배열 길이와 정수 길이를 상수인 int 변수로 선언했음.
    따라서 매개변수도 필요 없어졌기 때문에, 매개변수를 삭제했음.

--- cppPractice/practice4/4-2.cpp
+++ cppPractice/practice4/4-2.cpp
@@ -7,23 +7,24 @@ struct Student {
     int scores[10];
 };
 
-void printExcellentStudent(Student& selected_student, int name_length, int score_length) {
+int const name_length = 20;
+int const score_count = 10;
+
+void printExcellentStudent(Student& const selected_student) {
     float result = 0;
-    for (int i = 0; i < score_length; i++) result += selected_student.scores[i];
-    result /= score_length;
+    for (int i = 0; i < score_count; i++) result += selected_student.scores[i];
+    result /= score_count;;
     
-    cout << "이름: " << selected_student.name << " (평균: " << result << "점) ";
+    cout << "이름: " << selected_student.name << " (평균: " << (int)result << "점) ";
 
     // 학생 수준 판별 후 출력
     cout << "결과: ";
-    if (result >= 90) cout << "우수";
-    else cout << "일반";
+    (result >= 90) ? cout << "우수" : cout << "일반";
     cout << "학생입니다!" << endl;
 }
 
 void addBonusPoint(Student& student, int bonus) {
-    int const score_length = 10;
-    for (int i = 0; i < score_length; i++) student.scores[i] += bonus;
+    for (int i = 0; i < score_count; i++) student.scores[i] += bonus;
 }
 
 int main() {
@@ -35,7 +36,7 @@ int main() {
     
     // 2. 보너스 전 전체 명단 출력
     cout << "--- [1차 성적 확인(전체)] ---" << endl;
-    for (int i = 0; i < 2; i++) printExcellentStudent(stdList[i], 20, 10);
+    for (int i = 0; i < 2; i++) printExcellentStudent(stdList[i]);
     
     // 3. 첫 번째 학생(stdList[0])에게 보너스 부여
     cout << "\n>> " << stdList[0].name << " 학생에게 보너스 10점 부여 중..." << endl;
@@ -43,6 +44,6 @@ int main() {
     
     // 4. 보너스 후 전체 명단 재확인
     cout << "\n--- [2차 성적 확인 (전체)] ---" << endl;
-    for (int i = 0; i < 2; i++) printExcellentStudent(stdList[i], 20, 10);
+    for (int i = 0; i < 2; i++) printExcellentStudent(stdList[i]);
     return 0;
 }
