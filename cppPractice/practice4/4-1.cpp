b560a93c8e3227df1b5c2f686b86a00955518396
chillbok <kylejunho@gmail.com>
2026-04-02 15:39:45 +0900

    4.1.0.1: 기초 코드 생성

---
+++ cppPractice/practice4/4-1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using std::cin; using std::cout; using std::endl;
+
+// 원 면적 출력
+double calculateArea(double radius) {
+    double result = 0;
+    return result;
+}
+
+// 삼각형 면적 출력
+double calculateArea(double base, double height) {
+    double result = 0;
+    return result;
+}
+
+// 사각형 면적 출력
+int calculateArea(int width, int height=-1) {
+    int result = 0;
+    return result;
+}
+
+int main() {
+    cout << "반지름 5.0dls 원: " << calculateArea(5.0) << endl;
+    cout << "밑변 4.0, 높이 3.0인 삼각형: " << calculateArea(4.0, 3.0) << endl;
+    cout << "가로 10, 세로 5인 직사각형: " << calculateArea(10, 5) << endl;
+    cout << "한 변이 10인 정사각형: " << calculateArea(10) << endl;
+    return 0;
+}
