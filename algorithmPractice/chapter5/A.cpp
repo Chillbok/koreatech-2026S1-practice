be539197435268dab646756b42d799f5db3f06df
chillbok <kylejunho@gmail.com>
2026-04-03 01:21:56 +0900

    algorithm(5.a): 완성

--- algorithmPractice/chapter5/A.cpp
+++ algorithmPractice/chapter5/A.cpp
@@ -3,28 +3,44 @@ using std::cin; using std::cout; using std::endl;
 #include <vector>
 using std::vector;
 
-void swap (int& a, int& b) {
+void swap_number (int& a, int& b) {
     int temp = a;
     a = b;
     b = temp;
 }
 
-int partition(vector<int>& arr, int low, int high) {
-    int pivot = arr[high];
-    int i = (low - 1);
-    
-    for (int j = low; j <= high - 1; j++) {
-        if (arr[j] <= pivot) {
-            i++;
-            swap(arr[i], arr[j]);
+int divide(vector<int>& arr, int low_idx, int high_idx) {
+    int pivot_idx = high_idx;
+    int last_moved_idx = low_idx;
+    for (int i = low_idx; i < high_idx; i++) {
+        if (arr[i] <= arr[pivot_idx]) {
+            // 현재 요소를 작은 요소 영역의 끝과 교환
+            swap_number(arr[i], arr[last_moved_idx]);
+            last_moved_idx++;
         }
+    }
+    // 마지막에 왼쪽으로 몰아넣은 값 바로 오른쪽에 pivot 넣기
+    swap_number(arr[last_moved_idx], arr[pivot_idx]);
+    // pivot 위치 알리기
+    return last_moved_idx;
+}
+
+void quick_sort(vector<int>& arr, int low_idx, int high_idx) {
+    if (low_idx < high_idx) {
+        // 피벗 인덱스 얻기
+        int pivot_idx = divide(arr, low_idx, high_idx);
+        quick_sort(arr, low_idx, pivot_idx - 1);
+        quick_sort(arr, pivot_idx + 1, high_idx);
     }
 }
 
 void test() {
     int size; cin >> size;
-    int numbers[size];
+    vector<int> numbers(size);
     for (int i = 0; i < size; i++) cin >> numbers[i];
+    quick_sort(numbers, 0, numbers.size()-1);
+    for (int i = 0; i < size; i++) cout << numbers[i] << " ";
+    cout << endl;
 }
 
 int main() {
