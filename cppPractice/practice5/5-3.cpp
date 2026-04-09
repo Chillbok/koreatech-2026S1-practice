fb1906db471b9392a77c34c4122ddcf20d250c75
chillbok <kylejunho@gmail.com>
2026-04-09 12:37:44 +0900

    인공지능의 조언을 받아 try-catch 사용

--- cppPractice/practice5/5-3.cpp
+++ cppPractice/practice5/5-3.cpp
@@ -10,6 +10,7 @@ public:
     float Sub(float a, float b) { ++count; return a - b; }
     float Mul(float a, float b) { ++count; return a * b; }
     float Div(float a, float b) {
+        // 인공지능에게 질문하여 값을 반환하지 않고 함수만 끝내는 법을 알게 되었음.
         if (b == 0) throw "[에러] 0으로 나눌 수 없습니다!";
         ++count;
         return a / b;
@@ -28,9 +29,15 @@ int main() {
     cout << "15.0 - 7.5 = " << result << endl;
     result = cal.Mul(4.0, 5.0);
     cout << "4.0 * 5.0 = " << result << endl;
-    result = cal.Div(10.0, 2.0);
-    cout << "10.0 / 2.0 = " << result << endl;
-    result = cal.Div(5.0, 0); 
+    try {
+        result = cal.Div(10.0, 2.0);
+        cout << "10.0 / 2.0 = " << result << endl;
+
+        // 여기에서 throw가 발생하고, 즉시 catch로 이동
+        result = cal.Div(5.0, 0); 
+    }
+    // throw로 던져진 문자열을 잡아서 출력
+    catch (const char* msg) { cout << msg << endl; }
 
     cout << "-----------------------" << endl;
     cal.ShowCount();
