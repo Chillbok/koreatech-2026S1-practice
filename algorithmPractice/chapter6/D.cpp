a8714002b2c86126f7b01057ad0d08d7f1b5fe13
chillbok <kylejunho@gmail.com>
2026-04-15 23:45:25 +0900

    algo(6.d): 테스트케이스 입력 부분까지 완성

--- algorithmPractice/chapter6/D.cpp
+++ algorithmPractice/chapter6/D.cpp
@@ -3,6 +3,14 @@ using std::cin; using std::cout; using std::endl;
 #include <vector>
 using std::vector;
 
+void test() {
+	int N; cin >> N;
+	vector<int> input(N);
+	for (int& x : input) cin >> x;
+}
+
 int main() {
+	int T; cin >> T;
+	while (T--) test();
 	return 0;
 }
