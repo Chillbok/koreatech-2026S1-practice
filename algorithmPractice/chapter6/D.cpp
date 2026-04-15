5bfefed80a5c58b5fc1a85bd6254c322d6aeff24
chillbok <kylejunho@gmail.com>
2026-04-15 23:49:36 +0900

    algo(6.d): check_same() 함수 완성
    첫번째, 세번째 매개변수를 더한 뒤 2로 나눈 값이 두번째 매개변수와 같으면 true, 아니면 false를 반환함.

--- algorithmPractice/chapter6/D.cpp
+++ algorithmPractice/chapter6/D.cpp
@@ -3,6 +3,12 @@ using std::cin; using std::cout; using std::endl;
 #include <vector>
 using std::vector;
 
+bool check_same(const int n1, const int n2, const int n3) {
+	int number = (n1 + n3) / 2;
+	// 둘이 같으면 true 반환
+	return (n2 == number) ? true : false;
+}
+
 void test() {
 	int N; cin >> N;
 	vector<int> input(N);
