1837216870188f44443f41f51d09c6af05ebb2aa
chillbok <kylejunho@gmail.com>
2026-04-16 00:41:39 +0900

    algo(6.d): 코드 완성

--- algorithmPractice/chapter6/D.cpp
+++ algorithmPractice/chapter6/D.cpp
@@ -3,18 +3,26 @@ using std::cin; using std::cout; using std::endl;
 #include <vector>
 using std::vector;
 #include <algorithm>
-using std::swap;
-
-bool check_same(const int n1, const int n2, const int n3) {
-	int number = (n1 + n3) / 2;
-	// 둘이 같으면 true 반환
-	return (n2 == number) ? true : false;
-}
+using std::sort;
 
 void test() {
 	int N; cin >> N;
 	vector<int> input(N);
 	for (int& x : input) cin >> x;
+	sort(input.begin(), input.end());
+	
+	vector<int> result(N);
+	int mid = (N + 1) / 2;
+	int small_idx = 0;
+	int large_idx = mid;
+	
+	for (int i = 0; i < N; ++i) {
+		if (i % 2 == 0) result[i] = input[small_idx++];
+		else result[i] = input[large_idx++];
+	}
+	
+	for (int i = 0; i < N; ++i) cout << result[i] << " ";
+	cout << endl;
 }
 
 int main() {
