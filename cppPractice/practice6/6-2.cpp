613a61c8c233184e75b4d757a983481c57f67edc
chillbok <kylejunho@gmail.com>
2026-04-16 22:47:39 +0900

    cpp(6.2.0.2): main 함수 작업 중
    이후 버전에서는 값 비교하고 더 싼 책을 찾는 코드를 만들 예정임.

--- cppPractice/practice6/6-2.cpp
+++ cppPractice/practice6/6-2.cpp
@@ -51,5 +51,17 @@ class Book {
 };
 
 int main() {
+	Book book1("C++ Programming", "홍릉출판사", 25000);
+	Book book2 = book1;
+	Book book3 = { "C++ Programming", "교보문고", 18000 };
+	
+	book2.update_price(18000).update_price(25000);
+	book3.update_price(30000).update_price(19000);
+	
+	cout << "Book 1 정보: "; book1.show_info();
+	cout << "Book 2 정보: "; book2.show_info();
+	cout << "Book 3 정보: "; book3.show_info();
+	
+	// 아래에 값 비교하고 더 싼 책을 찾는 코드 만들 예정
 	return 0;
 }
