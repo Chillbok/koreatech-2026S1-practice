76f51f2d039bba9a75b4627c54a3891870e3faae
chillbok <kylejunho@gmail.com>
2026-04-16 11:36:38 +0900

    cpp(6.1.0.1): 실습 문제에서 이상한 점들을 고쳐서 완성

---
+++ cppPractice/practice6/6-1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using std::cin; using std::cout; using std::endl;
+#include <cstring>
+using std::strcpy;
+
+class Book {
+	private:
+	char *title;
+
+	public:
+	// 매개변수가 제대로 선언되지 않았음.
+	// Book(char*);
+	Book(const char* str = NULL);
+	~Book() {
+		delete title;
+	}
+	void showtitle(void);
+};
+
+Book::Book(const char *str = NULL) {
+	int len;
+	len = strlen(str);
+	title = new char[len + 1];
+	// 예제로 제공된 strcpy_s는 맥의 clang 컴파일러에서 인식하지 못하므로, strcpy를 사용하였음.
+	strcpy(title, str);
+}
+
+void Book::showtitle(void) {
+	cout << title << endl;
+}
+
+// C++ 표준에서는 더 이상 void main()을 지원하지 않는다고 알고 있음. 따라서 int main()으로 수정함.
+int main(void) {
+	Book book1("A C++ Programming");
+	Book book2 = book1;
+	
+	book1.showtitle();
+	book2.showtitle();
+	return 0;
+}
