a113f784f1a6696ea4f1305731d7465892ce5a3f
chillbok <kylejunho@gmail.com>
2026-04-28 14:02:23 +0900

    B.cpp 작성중

---
+++ algorithmPractice/chapter7/B.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using std::cin; using std::endl; using std::cout;
+#include <vector>
+using std::vector;
+
+void test() {
+	
+}
+
+int main() {
+	int T; cin >> T;
+	while (T--) test();
+	return 0;
+}
