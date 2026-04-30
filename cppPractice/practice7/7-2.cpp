2d6b7020430e8a68565ae83bcb03ba9633a33929
chillbok <kylejunho@gmail.com>
2026-04-30 12:02:53 +0900

    cpp(7-2): 클래스 생성자, 소멸자 구현 완료
    클래스의 생성자와 소멸자를 구현했음.

--- cppPractice/practice7/7-2.cpp
+++ cppPractice/practice7/7-2.cpp
@@ -3,6 +3,30 @@ using std::cin; using std::cout; using std::endl;
 #include <vector>
 using std::vector;
 
+class ObjectTracker {
+private:
+	static int currentCount;
+	static int totalCount;
+	int id;
+public:
+	ObjectTracker();
+	~ObjectTracker();
+	void ShowStatus();
+};
+
+int ObjectTracker::currentCount = 0;
+int ObjectTracker::totalCount = 0;
+
+ObjectTracker::ObjectTracker() {
+	currentCount++; totalCount++;
+	id = totalCount;
+	cout << "[ID: " << id << "] 객체 생성 (" << "현재 생존: " << currentCount << "개)" << endl;
+}
+ObjectTracker::~ObjectTracker() {
+	currentCount--;
+	cout << "[ID: " << id << "] 객체 소멸 (현재 생존: " << currentCount << "개)" << endl;
+}
+
 int main() {
 	return 0;
 }
