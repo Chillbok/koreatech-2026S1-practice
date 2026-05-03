e0c26159fce5b18800d4453d7bf0d5ba668160b7
chillbok <kylejunho@gmail.com>
2026-05-03 22:45:55 +0900

    cpp(7-3): 코드 완성

--- cppPractice/practice7/7-3.cpp
+++ cppPractice/practice7/7-3.cpp
@@ -10,7 +10,9 @@ private:
 	int face;
 public:
 	int get_face() const {return face;}
-	void roll() { face = rand() % 6 + 1; }
+	void roll() {
+		face = rand() % 6 + 1;
+	}
 };
 
 class Data {
@@ -19,6 +21,11 @@ private:
 	int count;
 	vector<int> dice_face_count;
 public:
+	// getter 함수들
+	int get_count() const { return count; }
+	int get_face_count(int idx) { return dice_face_count[idx]; }
+
+	// 생성자
 	Data() {
 		total = 0;
 		count = 0;
@@ -29,35 +36,37 @@ public:
 		++dice_face_count[n-1];
 		++count;
 	}
-	float get_face_average(int face_number) {
-		return (float)dice_face_count[face_number-1] / (float)count * 100;
+	float get_face_average(int idx) {
+		return (float)dice_face_count[idx] / (float)count * 100;
 	}
 	float get_average() {
 		float fl_total = total;
 		float fl_count = count;
 		return fl_total / fl_count;
 	}
-	int get_count() { return count; }
 };
 
 int main() {
 	srand((unsigned int)time(NULL));
 	Dice d1;
 	Data data;
-	
+	float average;
+
 	for (int i = 0; i < 10; ++i) {
 		d1.roll();
 		data.add_number(d1.get_face());
+		average = data.get_average();
+		cout << data.get_count() << "번째 주사위 면은 = " << d1.get_face() << " . 평균값 : " << data.get_average() << endl;
 	}
-	float average = data.get_average();
 	
+
 	while (average < 3.45 || average > 3.64) {
 		d1.roll();
 		data.add_number(d1.get_face());
 		average = data.get_average();
+		cout << data.get_count() << "번째 주사위 면은 = " << d1.get_face() << " . 평균값 : " << average << endl;
 	}
-	cout << "횟수: " << data.get_count() << " " << "평균: " << average << endl;
-	cout << "각 주사위 면 별 등장 확률: ";
-	for (int i = 0; i < 6; ++i) cout << i + 1 << "번: " << data.get_face_average(i+1) << "%" << endl;
+	for (int i = 0; i < 6; ++i) cout << i + 1 << "면: " << data.get_face_count(i) << "번이고, 확률은: " << data.get_face_average(i)<< endl;
+	cout << data.get_count() << "번 던졌을 때 평균값은 " << average << "입니다." << endl;
 	return 0;
 }
