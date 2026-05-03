c324c0454dbea2f7d27baa076c930d5184e3e395
chillbok <kylejunho@gmail.com>
2026-05-03 18:27:49 +0900

    cpp(7-3): 변수 평균 구하는 것까지 구현

--- cppPractice/practice7/7-3.cpp
+++ cppPractice/practice7/7-3.cpp
@@ -24,18 +24,30 @@ public:
 		total += n;
 		++count;
 	}
-	float get_average() { return total / count; }
+	float get_average() {
+		float fl_total = total;
+		float fl_count = count;
+		return fl_total / fl_count;
+	}
+	float get_count() { return count; }
 };
 
 int main() {
 	srand((unsigned int)time(NULL));
 	Dice d1;
-	
 	Data data;
 	
 	for (int i = 0; i < 10; ++i) {
 		d1.roll();
 		data.add_number(d1.get_face());
 	}
+	float average = data.get_average();
+	
+	while (average < 3.45 || average > 3.64) {
+		d1.roll();
+		data.add_number(d1.get_face());
+		average = data.get_average();
+	}
+	cout << "횟수: " << data.get_count() << "평균: " << average << endl;
 	return 0;
 }
