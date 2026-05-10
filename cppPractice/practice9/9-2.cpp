a7c546e2fecfff5cdcefef7a75b60de1baab9819
Chillbok <45466997+Chillbok@users.noreply.github.com>
2026-05-10 21:00:34 +0900

    cpp(9-2): 출력 횟수 저장 기능 추가
    vector<int> 변수를 사용해 구현함.
    이제 generator() 함수 선언 과정에서, 번호가 결정되면 해당하는 번호 위치에 있는 벡터에 1이 더해짐.
    
    벡터 변수는 main() 함수에서 생성되며, 변수를 생성한 뒤 0으로 초기화하는 작업이 필수적임.

--- cppPractice/practice9/9-2.cpp
+++ cppPractice/practice9/9-2.cpp
@@ -3,6 +3,8 @@ using std::cin; using std::cout; using std::endl;
 #include <cstdlib>
 #include <typeinfo>
 #include <ctime>
+#include <vector>
+using std::vector;
 
 class Shape {
 public:
@@ -39,15 +41,19 @@ public:
 	void draw() {}
 };
 
-Shape *generator() {
+Shape *generator(vector<int>& counts) {
 	switch(rand() % 4) {
 		case 0:
+			counts[0]++;
 			return new Line;
 		case 1:
+			counts[1]++;
 			return new Rectangle;
 		case 2:
+			counts[2]++;
 			return new Triangle;
 		case 3:
+			counts[3]++;
 			return new NullShape;
 	}
 	return NULL;
@@ -58,16 +64,21 @@ int main() {
 	Shape *p;
 	
 	int max_count = 10;
+
+	vector<int> shape_counts(4);
+	for (int& x : shape_counts) x = 0;
 	
 	cout << "=== " << max_count << "개의 Shape 생성을 시작합니다 ===" << endl << endl;
 	for (int i = 0; i < max_count; ++i) {
-		p = generator();
+		p = generator(shape_counts);
 		cout << "[" << i + 1 << "번째 생성 객체: class " << typeid(*p).name() << endl;
 		p->draw();
 		cout << "--------------------------------------" << endl;
 	}
 
 	delete p;
+	
+	for (int i = 0; i < shape_counts.size(); ++i) cout << shape_counts[i] << endl;
 
 	return 0;
 }
