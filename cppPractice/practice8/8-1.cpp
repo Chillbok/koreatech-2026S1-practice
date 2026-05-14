dce3a004ddd2a766d0a44828771421a04a89be82
Chillbok <45466997+Chillbok@users.noreply.github.com>
2026-05-14 16:01:53 +0900

    cpp(8-1): 가독성을 위해 클래스 함수 구현 위치 수정
    클래스 내부에서는 선언만 하고, 클래스 외부에서 구현을 하는 중이었음.
    가독성이 떨어진다고 판단하여 클래스 내부에서 구현까지 하도록 수정함.

--- cppPractice/practice8/8-1.cpp
+++ cppPractice/practice8/8-1.cpp
@@ -6,35 +6,31 @@ class NameCard {
 protected:
     char* name;
     char* phone_number;
-    
-    char* changeCharTo(const char* s);
+
+    char* changeCharTo(const char* s) {
+        if (!s) return nullptr;
+        char* temp = new char[strlen(s) + 1];
+        strcpy(temp, s);
+        return temp;
+    }
+
 public:
     // 기본 생성자
-    NameCard(const char* name);
+    NameCard(const char* name) {
+        this->name = changeCharTo(name);
+    }
+
     // 소멸자
-    ~NameCard();
-    void showInfo() const;
+    ~NameCard() {
+        delete[] name;
+        delete[] phone_number;
+        cout << "[System] NameCard(Base) 자원 해제 완료" << endl;
+    }
+
+    void showInfo() const {
+        cout << "이름: " << name << endl;
+    }
+
     // getter 함수들
     const char* getName() const { return name; }
 };
-
-char* NameCard::changeCharTo(const char* s) {
-    if (!s) return nullptr;
-    char* temp = new char[strlen(s) + 1];
-    strcpy(temp, s);
-    return temp;
-}
-
-NameCard::NameCard(const char* name) {
-    this->name = changeCharTo(name);
-}
-
-NameCard::~NameCard() {
-    delete[] name;
-    delete[] phone_number;
-    cout << "[System] NameCard(Base) 자원 해제 완료" << endl;
-}
-
-void NameCard::showInfo() const {
-    cout << "이름: " << name << endl;
-}
