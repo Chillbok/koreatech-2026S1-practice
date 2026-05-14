f6fadf5b518c2f846f2875eae93ccb858f4238b1
Chillbok <45466997+Chillbok@users.noreply.github.com>
2026-05-14 16:33:20 +0900

    feat(cpp8-1): RankNameCard 클래스 완성
    부모 클래스인 NameCard의 함수를 상속받아 만들어짐.
    따라서 오버라이드를 제대로 진행하기 위해서, showInfo() 함수와 소멸자에 virtual 키워드를 붙였음.

--- cppPractice/practice8/8-1.cpp
+++ cppPractice/practice8/8-1.cpp
@@ -21,13 +21,13 @@ public:
     }
 
     // 소멸자
-    ~NameCard() {
+    virtual ~NameCard() {
         delete[] name;
         delete[] phone_number;
         cout << "[System] NameCard(Base) 자원 해제 완료" << endl;
     }
 
-    void showInfo() const {
+    virtual void showInfo() const {
         cout << "이름: " << name << endl;
     }
 
@@ -35,8 +35,32 @@ public:
     const char* getName() const { return name; }
 };
 
+class RankNameCard : public NameCard {
+private:
+    char* rank = nullptr;
+public:
+    RankNameCard(const char* name, const char* rank) : NameCard(name) {
+        this->rank = changeCharTo(rank);
+    }
+    ~RankNameCard() {
+        delete[] rank;
+    }
+    
+    void showInfo() const override {
+        cout << "이름: " << name << " | 직급: " << rank << endl;
+    }
+    
+    // RTTI 실습을 위한 자식전용함수
+    void checkBonus() const {
+        cout << " -> [알림: 본인 확인 완료. 연말 특별 보너스 지급 대상자입니다.]" << endl;
+    }
+};
+
 int main() {
     NameCard junho("이준호");
     junho.showInfo();
+    
+    RankNameCard junho_kim("김준호", "CEO");
+    junho_kim.showInfo();
     return 0;
 }
