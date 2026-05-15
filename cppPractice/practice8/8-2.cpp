26f7bfc5ca337c4c609bc8a684d530a2355bd9d5
Chillbok <45466997+Chillbok@users.noreply.github.com>
2026-05-15 09:11:38 +0900

    feat(cpp8-2): 제시된 예제대로 Account 클래스 완성
    단, Account 클래스는 추후 작성할 LAccount 클래스의 부모 클래스이므로, 원활한 상속을 위해서 public 접근 지정자가 적용되지 않은 변수들을 protected로 지정함.

--- cppPractice/practice8/8-2.cpp
+++ cppPractice/practice8/8-2.cpp
@@ -1,5 +1,24 @@
+#include <cstring>
 #include <iostream>
 using std::cin; using std::cout; using std::endl;
+#include <cstring>
+
+class Account {
+protected:
+    int acc_num; // 계좌 번호
+    int balance; // 계좌 잔액
+    char name[20]; // 이름
+public:
+    Account(int _acc_num, int _balance, const char* _name) {
+        acc_num = _acc_num;
+        balance = _balance;
+        strcpy(name, _name);
+    }
+    void ShowData(void) {
+        cout << "계좌번호: " << acc_num << endl;
+        cout << "계좌잔액: " << balance << endl;
+    }
+};
 
 int main() {
 	return 0;
